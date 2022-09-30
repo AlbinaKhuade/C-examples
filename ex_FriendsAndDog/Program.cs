4ad5cbab2676480aab10658e20b5c848b1180d2c
Albina <albine89@mail.ru>
2022-09-30 15:57:08 +0300

    ex_FriendsAndDog

---
+++ ex_FriendsAndDog/Program.cs
@@ -0,0 +1,26 @@
+﻿int count = 0;
+int dist = 10000;
+int FirstFriendSpeed = 1;
+int SecondFriendSpeed = 2;
+int DogSpeed = 5;
+int Friend = 2;
+int time = 0;
+
+while (dist > 10)
+{
+    if (Friend == 1)
+    {
+        time = dist / (FirstFriendSpeed + DogSpeed);
+        Friend = 2;
+    }
+    else
+    {
+        time = dist / (SecondFriendSpeed + DogSpeed);
+        Friend = 1;
+    }
+    dist = dist - (FirstFriendSpeed + SecondFriendSpeed) * time;
+    count++;
+}
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.Write(" раз");
